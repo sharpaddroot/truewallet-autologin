2744ccdd413633606e8f2c58c3d981b26f73d0c0
NAVy DESIGn <40313260+sharpaddroot@users.noreply.github.com>
2020-07-21 21:22:27 +0700

    Update access_token.php

--- access_token.php
+++ access_token.php
@@ -2,7 +2,7 @@
   require "twapi/truewallet.php";
   require "system/conn.php";
 
-  $tw = new TrueWalletClass("0906571766pumzsp@truemoneywallet.com", "o871651277");//Login
+  $tw = new TrueWalletClass("กรอก email", "กรอก password");//Login
 
   $sql_access_token = "SELECT * FROM access_token WHERE token_id = 1";
   $access_token_query = mysqli_query($dbconnect,$sql_access_token);
