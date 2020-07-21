96e1fae0fdd5b610d974b953f86a91cbb3aa5b1a
NAVy DESIGn <40313260+sharpaddroot@users.noreply.github.com>
2020-07-21 21:22:51 +0700

    Update requestlogin.php

--- requestlogin.php
+++ requestlogin.php
@@ -1,7 +1,7 @@
 <?php
   require "twapi/truewallet.php";
   
-  $tw = new TrueWalletClass("0906571766pumzsp@truemoneywallet.com", "o871651277");//Login
+  $tw = new TrueWalletClass("กรอก email", "กรอก password");//Login
   
   $test = $tw->RequestLoginOTP();//เป็นฟังชั่นขอ otp
   print_r($test['code']);
